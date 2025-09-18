5a61e3ca38273185c56b5f90220d385350ec08a2
Sultan <117449289+sultanymbae@users.noreply.github.com>
2025-09-18 10:59:26 +0600

    add lab13

---
+++ lab13/lab13.sql
@@ -0,0 +1,16 @@
+select * from students;
+insert into students (name, email, faculty) values ('darina', 'darina@example.com', 'sfw-123')
+insert into students (name, email, faculty) values ('atai', 'atai@example.com', 'sfw-123')
+insert into students (name, email, faculty) values ('kuka', 'kuka@example.com', 'las-124')
+insert into students (name, email, faculty) values ('bekzat', 'bekzat@example.com', 'ba-123')
+
+select email 
+from students; 
+
+select name 
+from students where name ~ '^[d]';
+
+select count(*) as total_students
+from students;
+
+
